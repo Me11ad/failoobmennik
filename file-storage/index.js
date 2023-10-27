199881dd041fa6c9a639d1c0b68141951eba7ecb
Me11ad <112916199+Me11ad@users.noreply.github.com>
2023-10-27 11:01:48 +0500

    Add files via upload

--- file-storage/index.js
+++ file-storage/index.js
@@ -1,46 +1,73 @@
-const dropZone = D.querySelector('div')
-const input = D.querySelector('input')
-let file
+((D, B, log = (arg) =>
+console.log(arg)) => {
+    const dropZone = D.querySelector('div');
+    const input = D.querySelector('input');
+    let file;
 
-dropZone.addEventListener('drop', ev =>{
-    ev.preventDefault()
+    D.addEventListener("dragover", (ev) => ev.preventDefault());
+    D.addEventListener("drop", (ev) => ev.preventDefault());
 
-    log(ev.dataTransfer)
+    dropZone.addEventListener('drop', ev =>{
+        ev.preventDefault()
 
-    file = ev.dataTransfer.files[0]
+        log(ev.dataTransfer)
 
-    log(file)
+        file = ev.dataTransfer.files[0]
 
-    handleFile(file)
-})
+        log(file)
 
-dropZone.addEventListener('click',() =>{
-    input.click()
-
-    input.addEventListener('change', () => {
-        log(input.files)
-
-    file = input.files[0]
-
-    log(file)
-
-    handleFile(file)
+        handleFile(file)
     })
-})
 
-const handleFile = file => {
-    dropZone.remove()
-    input.remove()
-    log(file.type)
-if(file.type === 'text/html' ||
-    file.type === 'text/css' ||
-    file.type === 'text/javascript')
-return;
-if (file.type === 'application/pdf') {
+    dropZone.addEventListener('click',() =>{
+        input.click()
+    
+        input.addEventListener('change', () => {
+            log(input.files)
+    
+        file = input.files[0]
+    
+        log(file)
+    
+        handleFile(file)
+        })
+    })
+
+    const handleFile = file => {
+        dropZone.remove()
+        input.remove()
+        log(file.type)
+    if(
+        file.type === 'text/html' ||
+        file.type === 'text/css' ||
+        file.type === 'text/javascript'
+        )
+    return;
+
+    if (file.type === 'application/pdf') {
     createIframe(file)
     return;
-}
-const type = file.type.replace(/\/.+/, '')
+    }
 
-log(type)
+    const type = file.type.replace(/\/.+/, '')
+
+    log(type)
+
+    switch (type)  {
+        case "image":
+            createImage(file);
+            break;
+        case "audio":
+            createAudio(file);
+            break;
+        case "video":
+            createVideo(file);
+            break;
+        case "text":
+            createText(file);
+            break;
+        
+        default
+    }
+}
 }
