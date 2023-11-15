efd6096bd33ce85c518804ee5d0ce31ffa4705e0
Me11ad <vlad505200483@gmail.com>
2023-11-15 12:33:10 +0500

    Доделать код в js

---
+++ uploader.js
@@ -0,0 +1,13 @@
+function printFiles(e) {
+    const files = e.target.files;
+    let output = "";
+    for (let i = 0; i < files.length; i++){
+        const file = files[i];
+        console.log(file);
+
+        output += "<li><p><strong>" + file.name + "</strong></p>";
+        output += "<p>Type: " + file.type || "n/a</p>";
+        output += "<p>Size: " + file.size + " bytes</p>";
+        output += "<p>"
+    } 
+}
